04598f4770d9b3d9c51ef5a1a1592a39dd2e1092
SmartBigGuru <smartbigguru@gmail.com>
2024-11-30 00:30:11 -0500

    fix deploy error

--- src/app/api/admin/events/route.ts
+++ src/app/api/admin/events/route.ts
@@ -13,6 +13,7 @@ import { Pool } from 'pg';
 export const dynamic = 'force-dynamic';
 
 
+
 const pool = new Pool({
   user: 'clark',
   host: '199.244.49.83',
@@ -20,6 +21,8 @@ const pool = new Pool({
   password: 'Bmw635csi#',
   port: 5432,
 });
+
+
 
 // Fetch events (all or single)
 // Fetch events (all or single)
