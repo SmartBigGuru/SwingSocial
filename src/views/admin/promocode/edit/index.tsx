aa0996fa93f29ff34ee5fb5a79e73608888e37dc
SmartBigGuru <smartbigguru@gmail.com>
2024-12-26 17:26:10 -0500

    fix promo code feature

--- src/views/admin/promocode/edit/index.tsx
+++ src/views/admin/promocode/edit/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
-import { Autocomplete, Button, CircularProgress, Dialog, DialogActions, DialogContent, Divider, FormControl, FormControlLabel, Grid,TextField } from "@mui/material"
+import { Autocomplete, Button, CircularProgress, Dialog, DialogActions, DialogContent, Divider, FormControl, FormControlLabel, Grid, TextField } from "@mui/material"
 import { toast } from "react-toastify";
 // MUI Imports
 export interface EditPromocodeHandle {
@@ -26,95 +26,95 @@ const EditPromocodeDialogue = forwardRef<EditPromocodeHandle, RefreshProps>((pro
     description: promocodeDetail?.Description || "",
     displayMessage: promocodeDetail?.DisplayMessage || "",
     freeDays: promocodeDetail?.FreeDays || "",
-    expireDate:promocodeDetail?.ExpireDate &&  promocodeDetail?.ExpireDate || "",
-    active:true
+    expireDate: promocodeDetail?.ExpireDate && promocodeDetail?.ExpireDate || "",
+    active: true
   });
 
   const updatePromoCode = async () => {
     // Ensure required fields are provided
-    const {id, promoCodeText, description, displayMessage, freeDays, expireDate } = promoCodeData;
+    const { id, promoCodeText, description, displayMessage, freeDays, expireDate } = promoCodeData;
     if (!promoCodeText || !freeDays || !expireDate) {
-        setEmptyError(true);
-        toast.error('Complete Promocode info!', {
-            autoClose: 5000,
-            type: 'error',
-        });
-        return;
+      setEmptyError(true);
+      toast.error('Complete Promocode info!', {
+        autoClose: 5000,
+        type: 'error',
+      });
+      return;
     }
 
     // Validate for any empty fields
-    const hasEmptyFields = [id,promoCodeText, freeDays, expireDate].some(field => field === '');
+    const hasEmptyFields = [id, promoCodeText, freeDays, expireDate].some(field => field === '');
     if (hasEmptyFields) {
-        setEmptyError(true);
-        toast.error('Complete Promocode info!', {
-            autoClose: 5000,
-            type: 'error',
-        });
-        return;
+      setEmptyError(true);
+      toast.error('Complete Promocode info!', {
+        autoClose: 5000,
+        type: 'error',
+      });
+      return;
     }
 
     try {
-        // Call the API with promo code data
-        const response = await fetch('/api/admin/promocode/update', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                id,
-                promoCodeText,
-                description,
-                displayMessage,
-                freeDays,
-                expireDate,
-                active:true
-            }),
-        });
+      // Call the API with promo code data
+      const response = await fetch('/api/admin/promocode/update', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id,
+          promoCodeText,
+          description,
+          displayMessage,
+          freeDays,
+          expireDate,
+          active: 1
+        }),
+      });
 
-        // Handle response
-        if (response.ok) {
-            const responseData = await response.json();
-            toast.success('Promo code added successfully!', {
-                autoClose: 5000,
-                type: 'success',
-            });
-            setOpenDialog(false);
-            console.log('Response:', responseData);
-        } else {
-            const errorData = await response.json();
-            toast.error(errorData.message || 'Failed to add promo code!', {
-                autoClose: 5000,
-                type: 'error',
-            });
-            console.error('Error Response:', errorData);
-        }
+      // Handle response
+      if (response.ok) {
+        const responseData = await response.json();
+        toast.success('Promo code added successfully!', {
+          autoClose: 5000,
+          type: 'success',
+        });
+        setOpenDialog(false);
+        console.log('Response:', responseData);
+      } else {
+        const errorData = await response.json();
+        toast.error(errorData.message || 'Failed to add promo code!', {
+          autoClose: 5000,
+          type: 'error',
+        });
+        console.error('Error Response:', errorData);
+      }
     } catch (error) {
-        console.error('Error submitting form:', error);
-        toast.error('An error occurred while adding the promo code!', {
-            autoClose: 5000,
-            type: 'error',
-        });
+      console.error('Error submitting form:', error);
+      toast.error('An error occurred while adding the promo code!', {
+        autoClose: 5000,
+        type: 'error',
+      });
     }
-};
-useImperativeHandle(ref, () => ({
-  open: () => {
-    setOpenDialog(true);
-  },
-}));
+  };
+  useImperativeHandle(ref, () => ({
+    open: () => {
+      setOpenDialog(true);
+    },
+  }));
 
-useEffect(() => {
-  if (promocodeDetail) {
-    setPromoCodeData({
-      id: promocodeDetail.Id || "",
-      promoCodeText: promocodeDetail.PromoCodeText || "",
-      description: promocodeDetail.Description || "",
-      displayMessage: promocodeDetail.DisplayMessage || "",
-      freeDays: promocodeDetail.FreeDays || "",
-      expireDate:promocodeDetail?.ExpireDate &&  promocodeDetail.ExpireDate || "",
-      active:true
-    });
-  }
-}, [promocodeDetail]);
+  useEffect(() => {
+    if (promocodeDetail) {
+      setPromoCodeData({
+        id: promocodeDetail.Id || "",
+        promoCodeText: promocodeDetail.PromoCodeText || "",
+        description: promocodeDetail.Description || "",
+        displayMessage: promocodeDetail.DisplayMessage || "",
+        freeDays: promocodeDetail.FreeDays || "",
+        expireDate: promocodeDetail?.ExpireDate && promocodeDetail.ExpireDate || "",
+        active: true
+      });
+    }
+  }, [promocodeDetail]);
   return (
     <Dialog
       open={openDialog}
@@ -122,66 +122,66 @@ useEffect(() => {
       fullWidth
       aria-labelledby='max-width-dialog-title'>
       <DialogContent >
-      <Grid container spacing={5}>
-      <Grid item xs={12}>
-        <TextField
-          fullWidth
-          name='promoCodeText'
-          label='Promo Code'
-          autoComplete='off'
-          placeholder='Enter Promo Code'
-          value={promoCodeData.promoCodeText}
-          onChange={e => setPromoCodeData({ ...promoCodeData, promoCodeText: e.target.value })}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <TextField
-          fullWidth
-          name='description'
-          label='Description'
-          autoComplete='off'
-          placeholder='Description'
-          value={promoCodeData.description}
-          onChange={e => setPromoCodeData({ ...promoCodeData, description: e.target.value })}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <TextField
-          fullWidth
-          name='displayMessage'
-          label='Display Message'
-          autoComplete='off'
-          placeholder='Display Message'
-          value={promoCodeData.displayMessage}
-          onChange={e => setPromoCodeData({ ...promoCodeData, displayMessage: e.target.value })}
-        />
-      </Grid>
-      <Grid item xs={6}>
-        <TextField
-          fullWidth
-          name='freeDays'
-          label='Free Days'
-          autoComplete='off'
-          placeholder='Enter Free Days'
-          value={promoCodeData.freeDays}
-          onChange={e => setPromoCodeData({ ...promoCodeData, freeDays: e.target.value })}
-        />
-      </Grid>
-      <Grid item xs={6}>
-        <TextField
-          fullWidth
-          name='expireDate'
-          label='Expire Date'
-          type='date'
-          autoComplete='off'
-          InputLabelProps={{
-            shrink: true,
-          }}
-          value={promoCodeData.expireDate}
-          onChange={e => setPromoCodeData({ ...promoCodeData, expireDate: e.target.value })}
-        />
-      </Grid>
-    </Grid>
+        <Grid container spacing={5}>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              name='promoCodeText'
+              label='Promo Code'
+              autoComplete='off'
+              placeholder='Enter Promo Code'
+              value={promoCodeData.promoCodeText}
+              onChange={e => setPromoCodeData({ ...promoCodeData, promoCodeText: e.target.value })}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              name='description'
+              label='Description'
+              autoComplete='off'
+              placeholder='Description'
+              value={promoCodeData.description}
+              onChange={e => setPromoCodeData({ ...promoCodeData, description: e.target.value })}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              name='displayMessage'
+              label='Display Message'
+              autoComplete='off'
+              placeholder='Display Message'
+              value={promoCodeData.displayMessage}
+              onChange={e => setPromoCodeData({ ...promoCodeData, displayMessage: e.target.value })}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              name='freeDays'
+              label='Free Days'
+              autoComplete='off'
+              placeholder='Enter Free Days'
+              value={promoCodeData.freeDays}
+              onChange={e => setPromoCodeData({ ...promoCodeData, freeDays: e.target.value })}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              name='expireDate'
+              label='Expire Date'
+              type='date'
+              autoComplete='off'
+              InputLabelProps={{
+                shrink: true,
+              }}
+              value={promoCodeData.expireDate}
+              onChange={e => setPromoCodeData({ ...promoCodeData, expireDate: e.target.value })}
+            />
+          </Grid>
+        </Grid>
 
       </DialogContent>
       <Divider />
