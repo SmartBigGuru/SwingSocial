7acb6591f9b8b8a58383d56ee08c6058bfc8fd49
faisalayazsolangi66 <faisalayazsolangi@gmail.com>
2025-01-15 21:20:36 +0500

    fix deploy issue

--- src/app/api/admin/email/route.tsx
+++ src/app/api/admin/email/route.tsx
@@ -1,6 +1,26 @@
-import { NextResponse } from 'next/server';
-import { ServerClient } from 'postmark';
 
+/**
+ * ! We haven't used this file in our template. We've used the server actions in the
+ * ! `src/app/server/actions.ts` file to fetch the static data from the fake-db.
+ * ! This file has been created to help you understand how you can create your own API routes.
+ * ! Only consider making API routes if you're planing to share your project data with other applications.
+ * ! else you can use the server actions or third-party APIs to fetch the data from your database.
+ */
+
+// Next Imports
+import { responsiveFontSizes } from '@mui/material';
+import { NextResponse } from 'next/server'
+import { ServerClient } from 'postmark';
+import { Pool } from 'pg';
+export const dynamic = 'force-dynamic';
+
+const pool = new Pool({
+  user: 'clark',
+  host: '199.244.49.83',
+  database: 'swingsocialdb',
+  password: 'Bmw635csi#',
+  port: 5432,
+});
 
 export async function POST(req: any) {
   try {
