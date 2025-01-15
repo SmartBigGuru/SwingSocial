55346956a6aac64915e122a3731eec1168f67f28
SmartBigGuru <smartbigguru@gmail.com>
2025-01-15 10:51:52 -0500

    add sending email feature

--- src/app/api/admin/email/route.tsx
+++ src/app/api/admin/email/route.tsx
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { ServerClient } from 'postmark';
 
+
 export async function POST(req: any) {
   try {
     const { targetSegment, templateId, templateName } = await req.json();
